4d7ff0a282fceef04b54fefd2320674fc954d7a5
gigcot <gigcot@gmail.com>
2026-06-24 18:57:07 +0900

    fix: DB stale 커넥션 재사용 방지

--- config/database.py
+++ config/database.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
